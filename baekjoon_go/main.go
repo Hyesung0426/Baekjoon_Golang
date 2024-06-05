94ce6c159384b1bc8c7dea56f2bdea13699b1f89
hyesung <shinhyesung0426@gmail.com>
2024-06-05 14:09:45 +0900

    new

--- baekjoon_go/main.go
+++ baekjoon_go/main.go
@@ -1,13 +1,7 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func main() {
-	var (
-		A int
-		B int
-	)
 
-	fmt.Scanf("%d %d", &A, &B)
-	fmt.Println(A + B)
 }
---
+++ baekjoon_go/solved/N1000.go
@@ -0,0 +1,13 @@
+package solved
+
+import "fmt"
+
+func main() {
+	var (
+		A int
+		B int
+	)
+
+	fmt.Scanf("%d %d", &A, &B)
+	fmt.Println(A + B)
+}
